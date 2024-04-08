6f84650a56609a01b587e121dceca92b904ffe09
Anchana <anchanareghu20@gmail.com>
2024-04-08 21:21:58 +0530

    commit to main

--- app/src/androidTest/java/com/example/colorpicker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/colorpicker/ExampleInstrumentedTest.kt
@@ -1,29 +1,24 @@
-package com.example.colorpicker;
+package com.example.colorpicker
 
-import android.app.Instrumentation;
-import android.content.Context;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import kotlin.Metadata;
-import kotlin.jvm.internal.Intrinsics;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.jvm.internal.Intrinsics
 
-@RunWith(AndroidJUnit4.class)
-@Metadata(
-        mv = {1, 9, 0},
-        k = 1,
-        d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u0005¢\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0007¨\u0006\u0005"},
-        d2 = {"Lcom/example/colorpicker/ExampleInstrumentedTest;", "", "()V", "useAppContext", "", "app_debugAndroidTest"}
-)
-public final class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        Instrumentation var10000 = InstrumentationRegistry.getInstrumentation();
-        Intrinsics.checkNotNullExpressionValue(var10000, "InstrumentationRegistry.getInstrumentation()");
-        Context appContext = var10000.getTargetContext();
-        Intrinsics.checkNotNullExpressionValue(appContext, "appContext");
-        Assert.assertEquals("com.example.colorpicker", appContext.getPackageName());
+    fun useAppContext() {
+        val var10000 = InstrumentationRegistry.getInstrumentation()
+        Intrinsics.checkNotNullExpressionValue(
+            var10000,
+            "InstrumentationRegistry.getInstrumentation()"
+        )
+        val appContext = var10000.targetContext
+        Intrinsics.checkNotNullExpressionValue(appContext, "appContext")
+        Assert.assertEquals("com.example.colorpicker", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/example/colorpicker/ColorFragmentKt.kt
@@ -0,0 +1,36 @@
+package com.example.colorpicker
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class ColorFragmentKt : Fragment() {
+    private lateinit var cardViewText: TextView
+    private lateinit var cardViewColor: CardView
+    private lateinit var viewModel: ColorPickerViewModelKt
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_color, container, false)
+        cardViewText = view.findViewById(R.id.cardView_text)
+        cardViewColor = view.findViewById(R.id.cardView_color)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ColorPickerViewModelKt::class.java)
+        viewModel.getSelectedColor().observe(viewLifecycleOwner
+        ) { value ->
+            cardViewText.text = value
+            cardViewText.setTextColor(Color.parseColor(value))
+            cardViewColor.setCardBackgroundColor(Color.parseColor(value))
+        }
+        return view
+    }
+}
